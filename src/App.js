d32fdc5fc986727668c4b818a98b1be075027ae9
ozodbek <ozodbekjumakulov89@gmail.com>
2024-12-08 05:49:16 +0500

    search

--- src/App.js
+++ src/App.js
@@ -35,10 +35,40 @@ function App() {
             Career Path.
           </h1>
           <p>We have something big for you who want to level up!</p>
-          <div className="search-bar">
-            <input type="text" placeholder="What do you want to learn..." />
-            <button>🔍</button>
+          <form className="max-w-md mx-auto ">
+        <label
+          for="default-search"
+          className="mb-2 text-sm font-medium text-gray-500 sr-only dark:text-white"
+        >
+          Search
+        </label>
+        <div className="relative">
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white "
+            placeholder="What do you want to learn..."
+            required
+          />
+          {/* Input search icons */}
+          <div className="text-white absolute end-2 bottom-3 search__div cursor-pointer">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
           </div>
+          {/* Input search icons */}
+        </div>
+      </form>
           <div className="suggestions">
             <span>Suggestions:</span>
             <button>UI UX Design</button>
